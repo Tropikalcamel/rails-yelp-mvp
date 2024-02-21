c87d5d63ce824e614efbca41b1c4f6003862f560
Roi Assayag <rockybb777@gmail.com>
2024-02-21 16:48:44 +0100

    restaurnts try

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+  # belongs_to :restaurant
+
+  # class Reviews < ApplicationRecord
+  #   has_many :restaurants
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+city_chicken = {name: "City Chicken", address: "Berlin", phone_number: 1234546565, category: "japanese"}
+mustafa = {name: "Mustafa's GD", address: "Berlin Mitte", phone_number: 1234546565, category: "french"}
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: 1234546565, category: "italian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 1234546565, category: "belgian"}
+pizza_west =  {name: "Pizza West", address: "68A Shoreditch High St, leeds E1 6PQ", phone_number: 1234546565, category: "belgian"}
+
+[city_chicken, mustafa, dishoom, pizza_east, pizza_west].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+
+end
+
+puts "#{Restaurant.count} restaurants in database"
+puts "Finished!"
